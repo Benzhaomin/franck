18b44724349db8599887f40d193c2fbc703e2b94
Ben <ben@wainei.net>
2015-07-23 22:18:39 +0200

    video.beautify tests

--- tests/test_model_video.py
+++ tests/test_model_video.py
@@ -3,51 +3,60 @@
 
 from franck.model.video import Video
 
+import os
 import unittest
+import json
 
 def build_video():
-  v = Video('')
-  v.json = {
-    "title": "Rocket League : du foot motoris\u00e9 \u00e0 l'essai en split-screen !",
-    "cover": "http://image.jeuxvideo.com/images/videos/gaming_live_images/r/o/rocket-league-pc-121917-1436453790-high.jpg",
-    "id": "2150407",
-    "url": "http://www.jeuxvideo.com/videos/gaming-live/433637/rocket-league-du-foot-motorise-a-l-essai-en-split-screen.htm",
-    "description": "\nNous connaissance sur le jeu.\n",
-    "timeline": "http://image.jeuxvideo.com/images/videos/gaming_live_images/r/o/rocket-league-pc-121917-1436453790-0000.jpg",
-    "sources": {
-      "720p": {"size": 0, "file": "http://video720.jeuxvideo.com/gaming-live/r/o/rocket-league-pc-121917-1436453790-720p.mp4"},
-      "272p": {"size": 0, "file": "http://video.jeuxvideo.com/gaming-live/r/o/rocket-league-pc-121917-1436453790-low.mp4"},
-      "400p": {"size": 0, "file": "http://videohd.jeuxvideo.com/gaming-live/r/o/rocket-league-pc-121917-1436453790-high.mp4"},
-      "1080p": {"size": 0, "file": "http://video1080.jeuxvideo.com/gaming-live/r/o/rocket-league-pc-121917-1436453790-1080p.mp4"}
-    },
-    "iframe": "http://www.jeuxvideo.com/videos/iframe/2150407"
-  }
-  return v
+  with open(os.path.join(os.path.dirname(__file__), 'files', 'video_json.json')) as jsondump:
+    v = Video('http://www.jeuxvideo.com/gaming-live/0001/00011717/kingdom-hearts-chain-of-memories-gameboy-advance-gba-sora-00000849.htm')
+    v.json = json.load(jsondump)
+    return v
+
+def get_video_config():
+  with open(os.path.join(os.path.dirname(__file__), 'files', 'video_config.json')) as jsondump:
+    return json.load(jsondump)
+    
+def get_video_info():
+  with open(os.path.join(os.path.dirname(__file__), 'files', 'video_info.json')) as jsondump:
+    return json.load(jsondump)
   
+# recursive ordering (dict/list with nested dict/list)
+def ordered(obj):
+    if isinstance(obj, dict):
+        return sorted((k, ordered(v)) for k, v in obj.items())
+    if isinstance(obj, list):
+        return sorted(ordered(x) for x in obj)
+    else:
+        return obj
   
 class TestVideoGetSource(unittest.TestCase):
-  # want default (1080p) has it
-  def test_get_source_default_quality_has_it(self):
-    v = build_video()
-    expected = 'http://video1080.jeuxvideo.com/gaming-live/r/o/rocket-league-pc-121917-1436453790-1080p.mp4'
-    actual = v.get_source()['file']
-    self.assertEqual(actual, expected)
     
-  # want default (1080p) has 720p max
+  # want default (1080p) has 400p max
   def test_get_source_default_quality_auto_select(self):
     v = build_video()
-    del v.json["sources"]["1080p"]
-    expected = 'http://video720.jeuxvideo.com/gaming-live/r/o/rocket-league-pc-121917-1436453790-720p.mp4'
+    expected = 'http://videohd.jeuxvideo.com/200705/kingdom_hearts_gba-00000849-high.mp4'
     actual = v.get_source()['file']
     self.assertEqual(actual, expected)
     
-  # want default (720p) has it
-  def test_get_source_720p_quality_has_it(self):
+  # want something else (272p) has it
+  def test_get_source_select_quality_has_it(self):
     v = build_video()
-    expected = 'http://video720.jeuxvideo.com/gaming-live/r/o/rocket-league-pc-121917-1436453790-720p.mp4'
-    actual = v.get_source('720p')['file']
+    expected = 'http://video.jeuxvideo.com/200705/kingdom_hearts_gba-00000849-low.mp4'
+    actual = v.get_source('272p')['file']
     self.assertEqual(actual, expected)
 
+class TestVideoBeautify(unittest.TestCase):
+  def test_beautify(self):
+    
+    v = Video('http://www.jeuxvideo.com/gaming-live/0001/00011717/kingdom-hearts-chain-of-memories-gameboy-advance-gba-sora-00000849.htm')
+    config = get_video_config()
+    info = get_video_info()
+    
+    expected = build_video().json
+    actual = v.beautify(config, info)
+    
+    self.assertEqual(ordered(actual), ordered(expected))
     
 if __name__ == '__main__':
   unittest.main()
