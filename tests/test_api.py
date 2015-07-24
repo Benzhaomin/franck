a8cbb645da36e9dc577aca4622250d3aba32f252
Ben <ben@wainei.net>
2015-07-24 21:15:25 +0200

    added franck.api tests

---
+++ tests/test_api.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import unittest
+from unittest.mock import patch
+
+from franck.api import videos
+from franck.api import video
+
+from franck.model.video import Video
+
+def build_video(url):
+  return Video(url)
+
+# franck.api.videos()
+class TestApiVideos(unittest.TestCase):
+  
+  # check that we request parsing and return a list of results
+  @patch('franck.api.parser.video_pages', return_value=['http://www.jeuxvideo.com/foo', 'http://www.jeuxvideo.com/bar', 'http://www.jeuxvideo.com/baz'])
+  @patch('franck.api.video', side_effect=build_video)
+  def test_api_videos(self, foo, bar):
+    expected = [Video('http://www.jeuxvideo.com/foo'), Video('http://www.jeuxvideo.com/bar'), Video('http://www.jeuxvideo.com/baz')]
+    actual = videos('http://www.jeuxvideo.com/list')
+    self.assertEqual(actual, expected)
+
+# franck.api.video()
+class TestApiVideo(unittest.TestCase):
+  
+  # check that we build a Video object and load it
+  def test_api_video(self):
+    with patch('franck.model.video.Video.load') as m:
+      expected = Video('http://www.jeuxvideo.com/foo')
+      actual = video('http://www.jeuxvideo.com/foo')
+      self.assertEqual(actual, expected)
+      m.assert_called_once_with()
+  
+if __name__ == '__main__':
+  unittest.main()
