a37a415b15c31cd5a696091e3e89af4247f17fd4
Ben <ben@wainei.net>
2015-08-10 15:02:54 +0200

    set the back-end url to localhost:8080, should be config-editable

--- http/html/js/franck.js
+++ http/html/js/franck.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
     $('form').submit(function(e) {
         e.preventDefault();
-        
+
         stopVideo();
         $("#results").html('<img class="loading" src="img/loading.svg"/>');
-        
+
         var url = $('input[name="url"]').val();
         url = encodeURIComponent(url);
-        
-        $.get("http://localhost:5000/videos/"+url, function(data, status) {
-            //console.log(data);
+
+        $.get("http://localhost:8080/videos/"+url, function(data, status) {
+            //console.log(data, status);
         })
         .done(function(data) {
             $("#results").html('');
-            
+
             data.videos.sort(function(a, b) {
                 return parseInt(a.id) < parseInt(b.id);
             });
-            
+
             $.each(data.videos, function(index, config) {
                 var v = new Video(config);
                 $("#results").append(v.render());
             });
-            
+
             $("#results").find("article:first-child").addClass("size-big");
         })
         .fail(function() {
@@ -33,28 +33,28 @@ $(document).ready(function() {
             $(window).resize();
         });
     });
-    
+
     $("a.preset").click(function(e) {
         e.preventDefault();
         e.stopPropagation();
-        
+
         $('input[name="url"]').val($(this).attr("href"));
         $('form').submit();
     });
-    
+
     $("#results").off("click").on("click", "ul.sources > li > a", function(e) {
         e.preventDefault();
         e.stopPropagation();
-        
+
         playVideo($(this).attr("href"));
     });
-    
+
     function playVideo(src) {
         $("#player > video").attr("src", src).get(0).play();
         $("#player").show();
         $("html, body").animate({ scrollTop: 0 }, "slow");
     }
-    
+
     function stopVideo() {
         $("#player > video").attr("src", '');
         $("#player").hide();
@@ -67,6 +67,6 @@ $(document).ready(function() {
             }, 200);
         }
     });
-    
+
     // debug: $('form').submit();
 });
